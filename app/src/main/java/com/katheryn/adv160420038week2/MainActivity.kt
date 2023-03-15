f55870e79cfb202a51c0e33c4c0b64f4196dcffc
ketketin <114962909+ketketin@users.noreply.github.com>
2023-03-15 14:48:48 +0700

    Navigation

--- app/src/main/java/com/katheryn/adv160420038week2/MainActivity.kt
+++ app/src/main/java/com/katheryn/adv160420038week2/MainActivity.kt
@@ -2,23 +2,34 @@ package com.katheryn.adv160420038week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
     private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        drawerLayout = findViewById(R.id.drawerlayout)
 
         navController =
             (supportFragmentManager.findFragmentById(R.id.hostFragment) as
                     NavHostFragment).navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+
+        val navView = findViewById<NavigationView>(R.id.navView)
+        NavigationUI.setupWithNavController(navView, navController)
+        bottomNav.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
+                || super.onSupportNavigateUp()
     }
 }
